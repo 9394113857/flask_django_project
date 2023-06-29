057a12446c467ab50c350476bd4ee4d293aad55a
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2023-06-29 19:59:02 +0530

    No wthe sqlite database is added and working successfully

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
-import requests
-
 import logging
 import os
+import sqlite3
 from datetime import date
 from logging.handlers import RotatingFileHandler
+
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -29,6 +30,35 @@ for filename in os.listdir(logs_dir):
             os.remove(filepath)
 
 
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect('calculator.db')
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS calculator_logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                operation TEXT,
+                num1 REAL,
+                num2 REAL,
+                result REAL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+
+
 @app.route('/')
 def home():
     logger.info('Home page accessed')
@@ -49,33 +79,48 @@ def calculate():
 
     logger.info(f'Result: {result}')
 
+    conn = create_connection()
+    if conn is not None:
+        create_table(conn)
+
+        try:
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO calculator_logs (operation, num1, num2, result)
+                VALUES (?, ?, ?, ?)
+            ''', (operation, num1, num2, result))
+            conn.commit()
+        except sqlite3.Error as e:
+            print(e)
+        finally:
+            conn.close()
+
     return render_template('result.html', result=result)
 
-#####################################
-
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
 
 @app.route('/app1/')
 def app1_view():
     response = requests.get('http://localhost:8000/app1/')
     return response.content
 
+
 @app.route('/app1/second/')
 def app1_second_view():
     response = requests.get('http://localhost:8000/app1/second/')
     return response.content
+
 
 @app.route('/app2/')
 def app2_view():
     response = requests.get('http://localhost:8000/app2/')
     return response.content
 
+
 @app.route('/app2/second/')
 def app2_second_view():
     response = requests.get('http://localhost:8000/app2/second/')
     return response.content
 
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
